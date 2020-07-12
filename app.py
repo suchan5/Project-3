3ea2619dcadbaf30e1fc1b0bb7123844ff513aaa
suchan5 <suchan5@hotmail.com>
2020-07-12 19:41:12 +0000

    Connected to correct DB

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ MONGO_URI = os.environ.get('MONGO_URL')
 client = pymongo.MongoClient(MONGO_URI)
 
 # define my db name
-DB_NAME = "sample_restaurants"
+DB_NAME = "cookbooks"
 
 # read in the SESSION_KEY variable from the operating system environment
 SESSION_KEY = os.environ.get('SESSION_KEY')
@@ -29,13 +29,26 @@ def home():
     return render_template('home.template.html')
 
 
-@app.route('/recipe/create')
-def create_recipe():
-    return render_template('create_recipe.template.html')
+@app.route('/recipe/submit')
+def submit_recipe():
+    return render_template('submit_recipe.template.html')
 
 
-@app.route('/recipe/create', methods=['POST'])
-def process_create_recipe():
+@app.route('/recipe/submit', methods=['POST'])
+def process_submit_recipe():
+    print(request.form)
+
+    recipe_title = request.form.get('recipe-title')
+    about_recipe = request.form.get('about-recipe')
+    ingredients = request.form.get('ingredients')
+    directions = request.form.get('directions')
+
+    client[DB_NAME].submittedRecipes.insert_one({
+        "title": recipe_title,
+        "about": about_recipe,
+        "ingredients": ingredients,
+        "directions": directions
+    })
     return "form received"
 
 
